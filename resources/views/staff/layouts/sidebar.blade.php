07bb8e3877abdf893620be0a8d06e09bb0f45856
phamvkhai20 <49799124+phamvkhai20@users.noreply.github.com>
2020-11-07 18:12:43 +0700

    update

--- resources/views/staff/layouts/sidebar.blade.php
+++ resources/views/staff/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
     <div id="m_ver_menu" class="m-aside-menu  m-aside-menu--skin-dark m-aside-menu--submenu-skin-dark "
         m-menu-vertical="1" m-menu-scrollable="1" m-menu-dropdown-timeout="500" style="position: relative;">
         <ul class="m-menu__nav  m-menu__nav--dropdown-submenu-arrow ">
-            <li class="m-menu__item  m-menu__item--active" aria-haspopup="true"><a href="index.html"
+            <li class="m-menu__item  m-menu__item--active" aria-haspopup="true"><a href="{{route('nha-truong.index')}}"
                     class="m-menu__link "><i class="m-menu__link-icon flaticon-line-graph"></i><span
                         class="m-menu__link-title"> <span class="m-menu__link-wrap"> <span
                                 class="m-menu__link-text">Dashboard</span>
