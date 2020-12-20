b3b385e3bb70b816ebdabcf0bb3787e59b2e65ad
phamvkhai20 <49799124+phamvkhai20@users.noreply.github.com>
2020-12-20 09:55:58 +0700

    update

--- resources/views/staff/nha-truong/layouts/sidebar.blade.php
+++ resources/views/staff/nha-truong/layouts/sidebar.blade.php
@@ -89,9 +89,12 @@
                     </ul>
                 </div>
             </li>
-
+            <li class="m-menu__section ">
+                <h4 class="m-menu__section-text" style="color:#868aa8;font-size:15px">Thông tin cho trẻ</h4>
+                <i class="m-menu__section-icon flaticon-more-v2"></i>
+            </li>
             <li class="m-menu__item  m-menu__item--submenu" aria-haspopup="true" m-menu-submenu-toggle="hover"><a href="javascript:;" class="m-menu__link m-menu__toggle"><i class="m-menu__link-icon la la-yc-square"></i><span class="m-menu__link-text">Quản lý thông báo
-                        học</span><i class="m-menu__ver-arrow la la-angle-right"></i></a>
+                        </span><i class="m-menu__ver-arrow la la-angle-right"></i></a>
                 <div class="m-menu__submenu "><span class="m-menu__arrow"></span>
                     <ul class="m-menu__subnav">
                         <li class="m-menu__item " aria-haspopup="true"><a href="{{route('nha-truong.thong-bao.index')}}" class="m-menu__link "><i class="m-menu__link-bullet m-menu__link-bullet--dot"><span></span></i><span class="m-menu__link-text">Danh sách các thông báo</span></a></li>
