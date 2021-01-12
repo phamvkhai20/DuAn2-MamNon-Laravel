c380a296087ac4988a57c02feaceaf19468fc1bc
hang1701-20 <=>
2021-01-12 23:23:04 +0700

    validate lien he

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -17,6 +17,17 @@ class ContactController extends Controller
     }
     public function saveAdd(Request $request)
     {
+        $request->validate([
+            'contact_name' => 'required',
+            'contact_phone' => 'required',
+            'contact_email' => 'required',
+            'detail' => 'required',
+        ],[
+            'contact_name.required' => 'Phải nhập họ tên',
+            'contact_phone.required' => 'Phải nhập số điện thoại',
+            'contact_email.required' => 'Phải nhập email',
+            'detail.required' => 'Phải nhập tin nhắn',
+        ]);
         $data = request()->all();
         ContacModel::create($data);
         return redirect()->route('web.lien-he');
